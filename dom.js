a0af78efc1df1006ac7517f32d3ff3a005e48e9b
Ramyasri <rnimmagadda@salesforce.com>
2020-01-17 13:47:53 -0800

    escaping single quote

--- dom.js
+++ dom.js
@@ -409,7 +409,8 @@ function _xmlEncoder(c){
 	return c == '<' && '&lt;' ||
          c == '>' && '&gt;' ||
          c == '&' && '&amp;' ||
-         c == '"' && '&quot;' ||
+	 c == '"' && '&quot;' ||
+	 c == "'" && '&apos;' ||
          '&#'+c.charCodeAt()+';'
 }
 
